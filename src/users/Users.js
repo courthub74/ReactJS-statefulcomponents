9e0ae67149b9b7690775b9f6d6face28f46d5106
Courdevelops <courdevelops@gmail.com>
2021-12-31 13:45:01 -0500

    first component

---
+++ src/users/Users.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import User from './User';
+
+class Users extends Component {
+    render() {
+        return (
+            <div>
+                <User/>
+                <User/>
+                <User/>
+                <User/>
+            </div>
+        )
+    }
+}
+
+export default Users;
